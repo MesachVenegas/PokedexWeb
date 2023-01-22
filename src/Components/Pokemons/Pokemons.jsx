eb37d59482decd9922366b390ca9b6ed99ab29e5
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-21 20:12:41 -0800

    add: view style order

--- src/Components/Pokemons/Pokemons.jsx
+++ src/Components/Pokemons/Pokemons.jsx
@@ -1,9 +1,24 @@
+import PokemonCard from '../PokemonCard/PokemonCard';
+import logo from '../../assets/imgs/pokemon-logo.png'
 import React from 'react';
+import axios from 'axios'
+import './pokemons.css'
 
 const Pokemons = () => {
+
+
+
     return (
-        <div>
-            <h2>Listado de pokemons</h2>
+        <div className='pokemons_container'>
+            <div className="header">
+                <img src={logo} alt="pokemon_logo" className='logo'/>
+                <h2>Listado de pokemons</h2>
+
+            </div>
+
+            <div className="cards_container">
+                <PokemonCard />
+            </div>
         </div>
     );
 };
