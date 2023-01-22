35e3d4820fec7b778b490c88d40b62ef6dab5edf
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-21 17:33:54 -0800

    change: extension file

--- src/Store/index.jsx
+++ src/Store/index.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userName } from "./slices/user.slice";
+import userNameSlice from "./slices/user.slice";
 
 export default configureStore({
     reducer: {
-        userName
+        userName: userNameSlice
     }
 })
