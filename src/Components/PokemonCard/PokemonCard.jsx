bec83d58d920b079a76c8d0b85778f355648472a
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-23 00:28:38 -0800

    add: functions to load pokemon data

--- src/Components/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonCard/PokemonCard.jsx
@@ -4,33 +4,64 @@ import { useEffect, useState } from 'react';
 import './pokemoncard.css'
 
 const PokemonCard = ({ pokemon }) => {
-    const [data, setData] = useState({})
-    const [types, setTypes] = useState('')
+    const [data, setData] = useState({});
+    const [name, setName] =  useState('')
+    const [types, setTypes] = useState('');
+    const [hp, setHp] = useState('');
+    const [attack, setAttack] = useState('')
+    const [defense, setDefense] = useState('');
+    const [speed, setSpeed] = useState('')
 
     useEffect( () =>{
         if(pokemon){
             axios.get(pokemon.url)
-                .then( res => setData(res.data))
+                .then( res =>{
+                    setData(res.data)
+                    capitalizeName(res.data.name)
+                    loadTypes(res.data.types)
+                    loadStats(res.data.stats)
+                })
                 .catch( res => console.log(res))
 
         }
     },[pokemon])
 
-    const loadTypes = () =>{
-        const long = data.types?.length;
+    const loadTypes = (typeList) =>{
+        setTypes(typeList?.map(type => type.type.name).join(' - '))
     }
 
-    loadTypes()
+    const capitalizeName = (name) =>{
+            setName(name?.charAt(0)?.toUpperCase() + name?.slice(1));
+    }
+
+    const loadStats = (stats) =>{
+        stats.forEach(stat => {
+            let statName =  stat.stat?.name;
+            switch(statName){
+                case 'hp':
+                    setHp(stat.base_stat);
+                case 'attack':
+                    setAttack(stat.base_stat)
+                case 'defense':
+                    setDefense(stat.base_stat)
+                case 'speed':
+                    setSpeed(stat.base_stat)
+                default:
+                    return
+            }
+        });
+    }
+
     return (
         <div className='pokemon_card'>
-            <h3>{data.name}</h3>
+            <h3>{name}</h3>
             <div className="data_container">
                 <ul className='pokemon_data'>
-                    <li>Types: </li>
-                    <li>Hp: </li>
-                    <li>Attack: </li>
-                    <li>Defense: </li>
-                    <li>Speed: </li>
+                    <li>Types: {types}</li>
+                    <li>Hp:<span className='bar'></span>{hp}</li>
+                    <li>Attack: {attack}</li>
+                    <li>Defense: {defense}</li>
+                    <li>Speed: {speed}</li>
                 </ul>
                 <figure className='pokemon_sprite'>
                     <img src={data.sprites?.front_default} alt="sprite-pokemon" />
