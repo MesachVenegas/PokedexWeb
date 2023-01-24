bc26c1ef463dd73c3b0df69ad22a9968833c3cd9
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-23 20:39:15 -0800

    add: function to filter by type and initialiaze search by id or name

--- src/Components/Pokemons/Pokemons.jsx
+++ src/Components/Pokemons/Pokemons.jsx
@@ -6,11 +6,12 @@ import axios from 'axios'
 import './pokemons.css'
 
 const Pokemons = () => {
+    const [pokemonTypes, setPokemonTypes] = useState([])
     const [pokemons, setPokemons] = useState([])
-    const [pagination, setPagination] = useState(null)
-    const [next, setNext] = useState("")
-    const [change, setChange] = useState(false)
-    const [title, setTitile] = useState("")
+    const [t, setChange] = useState(false)
+    const [type, setType] = useState("")
+    const [toSearch, setToSearch] = useState('')
+
 
     const getPokemons = async () =>{
         await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=18')
@@ -20,8 +21,15 @@ const Pokemons = () => {
             .catch(error => console.log(error))
     }
 
-    const getPokemonsByType = async () =>{
-        await axios.get('https://pokeapi.co/api/v2/type/1/')
+    const getPokemonTypes = async () =>{
+        await axios.get(`https://pokeapi.co/api/v2/type/`)
+        .then( res => setPokemonTypes(res.data.results))
+        .catch( error => console.log(error))
+    }
+
+    // Obtengo el listado de pokemons por tipo
+    const getPokemonsByType = async (type) =>{
+        await axios.get(`https://pokeapi.co/api/v2/type/${type}/`)
             .then(res =>{
                 setPokemons(res.data.pokemon)
                 setChange(true)
@@ -29,8 +37,16 @@ const Pokemons = () => {
             .catch( error => console.log( error))
     }
 
+    const searchPokemon = async (name) =>{
+        console.log(name)
+        await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+            .then( res => console.log(res.data))
+            .catch(error => console.log(error))
+    }
+
+    // se encarga del renderizado de la lista de pokemon según el termino de busqueda.
     const loadPokemons = () =>{
-        if (!change) {
+        if (!t) {
             return pokemons.results?.map(pokemon => {
                 return(<PokemonCard key={pokemon.name} pokemon={pokemon} />)
             })
@@ -42,18 +58,46 @@ const Pokemons = () => {
         }
     }
 
+
     useEffect( () =>{
-        // getPokemons()
-        getPokemonsByType()
-    },[])
+        getPokemonTypes()
+        if(type){
+            getPokemonsByType(type)
+        }else{
+            getPokemons()
+        }
+    },[type])
 
 
     return (
         <div className='pokemons_container'>
             <div className="header">
-                <img src={logo} alt="pokemon_logo" className='logo'/>
-                <h2>Listado de pokemons</h2>
-
+                <div className="header">
+                    <img src={logo} alt="pokemon_logo" className='logo'/>
+                    <h2>Welcome! Trainer name</h2>
+                </div>
+                <div className="search">
+                    <label htmlFor="search">Search </label>
+                    <input
+                        id='search'
+                        type="search"
+                        value={toSearch}
+                        placeholder='Name or Id'
+                        onChange={e => setToSearch(e.target.value)}
+                    />
+                    <button onClick={ () => searchPokemon(toSearch) }>Search</button>
+                </div>
+                <div className="filter_bar">
+                    <label htmlFor="types">Filter by Types: </label>
+                    <select id="types" onChange={e => setType(e.target.value)}>
+                        <option key='default_option' value={null} defaultChecked>Seleccionar</option>
+                        {
+                            pokemonTypes?.map( type =>(
+                                <option key={type.name} value={type.name}>{type.name}</option>
+                            ))
+                        }
+                    </select>
+                </div>
             </div>
 
             <div className="cards_container">
