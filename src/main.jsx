9423ffd49d740c53f84104be62e7742b4ac18dcd
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-25 15:28:40 -0800

    changes: store

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import App from './App'
+import store from './store'
 import React from 'react'
-import Store from './store'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import './index.css'
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <Provider store={Store}>
+        <Provider store={store}>
             <App />
         </Provider>
     </React.StrictMode>
