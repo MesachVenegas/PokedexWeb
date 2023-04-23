f248d42935d01acbf4fc2515c01abf5831522f12
MesachVenegas <mesach.venegas@hotmail.com>
2023-04-23 16:12:36 -0700

    changes: add description of movement and stats

--- src/Components/movementDetail/MovementDetail.jsx
+++ src/Components/movementDetail/MovementDetail.jsx
@@ -8,23 +8,42 @@ const MovementDetail = ({url}) => {
     const [pp, setPP] = useState(null);
     const [description, setDescription] = useState(null);
     const [effect, setEffect] = useState(null);
-    const [accuracy, setAccuracy] = useState(null)
-    const [name, setName] = useState(null)
-    const [power, setPower] = useState(null)
-    const [damageType, setDamageType] = useState(null)
-    const [concurseType, setConcurseType] = useState(null)
+    const [accuracy, setAccuracy] = useState(null);
+    const [name, setName] = useState(null);
+    const [power, setPower] = useState(null);
+    const [damageType, setDamageType] = useState(null);
+    const [concurseType, setConcurseType] = useState(null);
+    const [category, setCategory] = useState(null);
+    const [drain, setDrain] = useState(null);
+    const [maxHits, setMaxHits] = useState(null);
+    const [minHits, setMinHits] = useState(null);
+    const [critRate , setCritRate] = useState(null);
+    const [maxTurns, setMaxTurns] = useState(null);
+    const [minTurns, setMinTurns] = useState(null);
+    const [flinch, setFlinch] = useState(null);
+    const [state, setState] = useState(null);
 
     const getData = async () =>{
         await axios.get(url)
             .then(res => {
+                // console.log(res.data)
                 setName(res.data.name)
                 setPP(res.data.pp);
                 setAccuracy(res.data.accuracy);
                 setType(res.data.type.name);
                 setPower(res.data.power);
-                setDamageType(res.data.damage_class.name)
-                setConcurseType(res.data.contest_type.name)
-
+                setDamageType(res.data.damage_class?.name)
+                setConcurseType(res.data.contest_type?.name)
+                setCategory(res.data.meta.category?.name)
+                setDrain(res.data.meta.drain)
+                setMaxHits(res.data.meta.max_hits)
+                setMinHits(res.data.meta.min_hits)
+                setCritRate(res.data.meta.crit_rate)
+                setMaxTurns(res.data.meta.max_turns)
+                setMinTurns(res.data.meta.min_turns)
+                setFlinch(res.data.meta.flinch_chance)
+                setState(res.data.meta.stat_chance)
+                console.log(res.data.meta)
                 // get effect description if language is english and not is null.
                 if(res.data.effect_entries.length > 0){
                     res.data.effect_entries?.map(entry =>{
@@ -64,7 +83,13 @@ const MovementDetail = ({url}) => {
                             Type: {type?.charAt(0).toLocaleUpperCase()+ type?.slice(1)}
                         </span>
                         <span className={concurseType}>
-                            Contest Type: {concurseType}
+                            Contest Type: { concurseType?.charAt(0).toLocaleUpperCase() + concurseType?.slice(1) }
+                        </span>
+                        <span>
+                            Damage Type: { damageType? damageType.charAt(0).toLocaleUpperCase() + damageType.slice(1) : "N/A" }
+                        </span>
+                        <span>
+                            Category: {category? category : "N/A"}
                         </span>
                         <span>
                             Accuracy: {accuracy ? accuracy : "N/A"}
@@ -73,14 +98,35 @@ const MovementDetail = ({url}) => {
                             Power: {power ? power : "N/A"}
                         </span>
                         <span>
-                            Damage Type: {damageType? damageType : "N/A"}
+                            PP: {pp}
                         </span>
                         <span>
-                            PP: {pp}
+                            Heal Drain: {drain ? drain : "N/A"}
+                        </span>
+                        <span>
+                            Crit Rate: {critRate ? critRate : "N/A"}
+                        </span>
+                        <span>
+                            Max Hits: {maxHits? maxHits : "N/A"}
+                        </span>
+                        <span>
+                            Min Hits: {minHits? minHits : "N/A"}
+                        </span>
+                        <span>
+                            Max Turns: {maxTurns? maxTurns : "N/A"}
+                        </span>
+                        <span>
+                            Min Turns: {minTurns? minTurns : "N/A"}
+                        </span>
+                        <span>
+                            Flinch Chance: {flinch? `${flinch}%` : "N/A"}
+                        </span>
+                        <span>
+                            Stat Chance: {state? `${state}%` : "N/A"}
                         </span>
                     </div>
                     <h4>Description:</h4>
-                    <p>{ description  ? description : "N/A"}</p>
+                    <p>{description ? description : "N/A"}</p>
                     <h4>Effect: </h4>
                     <p>{effect ? effect : "N/A"}</p>
                 </motion.div>
